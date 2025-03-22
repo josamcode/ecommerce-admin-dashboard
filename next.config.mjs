0d2a582895783a37d0e7f76bbaa4db63651f8f1f
josamcode <gergessamuel100@gmail.com>
2025-03-23 01:36:06 +0200

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,14 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["via.placeholder.com", "images.unsplash.com", "localhost"],
+    domains: [
+      "via.placeholder.com",
+      "images.unsplash.com",
+      "localhost",
+      "eastern-maryjane-josamcode-baebec38.koyeb.app",
+    ],
   },
   experimental: {
-    turbo: false,
+    turbo: {},
   },
 };
 
